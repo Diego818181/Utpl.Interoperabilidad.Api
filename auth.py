a66cef83bd6ad72faebf5c40037a983106442de0
Diego <sarmiento.df@gmail.com>
2023-07-28 13:16:13 -0500

    ajuste de contraseña

--- auth.py
+++ auth.py
@@ -8,6 +8,6 @@ def authenticate(credentials: HTTPBasicCredentials):
     # Aquí puedes verificar las credenciales y realizar la autenticación
     # Por ejemplo, verificar si el usuario y la contraseña son válidos en tu sistema
     # Si no son válidos, puedes lanzar una excepción HTTPException con el código de estado 401 (No autorizado)
-    if not (username == "admin" and password == "admin"):
+    if not (username == "admin" and password == "admin123"):
         raise HTTPException(status_code=401, detail="Credenciales inválidas")
     return username
